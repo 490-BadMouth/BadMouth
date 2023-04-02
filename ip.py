7700ac36ba1284e8c2a0e085ce5a294f7a803842
David George <david.george@student.csulb.edu>
2023-04-02 16:04:08 -0700

    python ip script

---
+++ ip.py
@@ -0,0 +1,7 @@
+# Python Program to Get IP Address
+import socket
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + IPAddr)
