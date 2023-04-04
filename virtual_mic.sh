190825a4ed67b0c55282a8f850536bf3e00901b2
dmg77 <david.george@student.csulb.edu>
2023-04-03 19:14:44 -0700

    saves module number in txt file

--- virtual_mic.sh
+++ virtual_mic.sh
@@ -6,4 +6,8 @@ if pactl list short modules | grep -q "source_name=Badmouth file=/tmp/Badmouth" 
 else
     echo 'Virtual Mic Created: Module #'
     pactl load-module module-pipe-source source_name=Badmouth file=/tmp/Badmouth format=s16le rate=44100 channels=1
-fi 
+    if test -f module_number.txt; then
+        rm -rf module_number.txt
+    fi
+    echo $(pactl list short modules | awk '/source_name=Badmouth/ {print $1}') >> module_number.txt
+fi
