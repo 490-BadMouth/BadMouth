6385038e575eacc4f88a9bd5dcafbe111b9d3ed1
dmg77 <david.george@student.csulb.edu>
2023-04-03 19:08:11 -0700

    no user input for bash

--- virtual_mic.sh
+++ virtual_mic.sh
@@ -3,14 +3,6 @@ if pactl list short modules | grep -q "source_name=Badmouth file=/tmp/Badmouth" 
     echo 'Virtual Mic Exists: Module #'
     module="$(pactl list short modules | awk '/source_name=Badmouth/ {print $1}')"
     echo "$module"
-    read -n1 -p 'Do you want to remove this module? (y for yes, n for no): ' input
-    if [[ $input == "Y" || $input == "y" ]]; then
-        echo ''
-        pactl unload-module $module
-        echo 'Module Removed'
-    else
-        echo ''
-    fi
 else
     echo 'Virtual Mic Created: Module #'
     pactl load-module module-pipe-source source_name=Badmouth file=/tmp/Badmouth format=s16le rate=44100 channels=1
