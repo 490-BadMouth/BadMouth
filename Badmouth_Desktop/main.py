d9f1f59d4a405a48ffab6cfc0dbda15f8b41f5ae
jsaladbar <justinsalazar217@gmail.com>
2023-04-05 01:27:54 -0700

    Updated GUI with integrated audio streaming v2?

--- Badmouth_Desktop/main.py
+++ Badmouth_Desktop/main.py
@@ -1,12 +1,19 @@
 import sys
+import socket
 from subprocess import Popen, PIPE, call
 import threading
-from PySide6.QtCore import Qt, Signal
+from PySide6.QtCore import Qt, Signal, QTimer
 from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget, QLabel
-from PySide6.QtGui import QColor, QPalette, QTextCursor
+from PySide6.QtGui import QColor, QPalette, QTextCursor, QPixmap
 
-BASH_SCRIPT = "bash vmic3.sh"  # Replace with the path to your bash script
-
+#Bash script for virtual mic setup through Port Audio
+BASH_SCRIPT = "bash vmic3.sh"  
+#Connection Info
+CORAL_IP = "192.168.100.2"  # Replace with the Google Coral's static IP address
+PORT = 5000
+CHUNK = 1024 * 4
+#Virtual Mic Path for pipe to connect
+PIPE_PATH = "/tmp/Badmouth"
 
 class MainWindow(QMainWindow):
     append_output_signal = Signal(str)
@@ -15,13 +22,22 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         # Set up the UI
+        print("Initializing UI...")
         self.init_ui()
 
         # Connect the append_output_signal to the append_output slot
         self.append_output_signal.connect(self.append_output)
 
         # Run the bash script in a separate thread to avoid blocking the GUI
+        print("Running Bash Script...")
         self.run_bash_script()
+
+        print("Attempting to connect stream...")
+        self.connect_stream()
+
+        self.running_indicator_init()
+
+
 
     def init_ui(self):
         central_widget = QWidget()
@@ -34,10 +50,20 @@ class MainWindow(QMainWindow):
 
         self.output_text = QTextEdit()
         self.output_text.setReadOnly(True)
+        self.output_text.setFixedSize(300,800)
         layout.addWidget(self.output_text)
+
+        # Image
+        self.image_label = QLabel()
+        pixmap = QPixmap("pain.png")
+        self.image_label.setPixmap(pixmap)
+        self.image_label.setScaledContents(True)
+        layout.addWidget(self.image_label)
 
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
+        # Set window size and aspect ratio
+        self.setFixedSize(600,800)
 
     def set_status_indicator_color(self, color_name):
         palette = self.status_indicator.palette()
@@ -45,10 +71,13 @@ class MainWindow(QMainWindow):
         palette.setColor(QPalette.Window, color)
         self.status_indicator.setPalette(palette)
 
-    #@Slot(str)
     def append_output(self, text):
         self.output_text.moveCursor(QTextCursor.End)
+        self.output_text.textCursor().deletePreviousChar()
+        self.output_text.moveCursor(QTextCursor.End)
         self.output_text.insertPlainText(text)
+        self.output_text.moveCursor(QTextCursor.End)
+        self.output_text.insertPlainText(self.running_indicator.text())
 
     def run_bash_script(self):
         #call(BASH_SCRIPT)
@@ -63,8 +92,69 @@ class MainWindow(QMainWindow):
                 break
             if output:
                 self.append_output_signal.emit(output)
-                self.set_status_indicator_color("green" if "SUCCESS" in output else "red")
 
+    def connect_stream(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(2)  # Added timeout to avoid blocking
+        self.process_poll_socket_connect = threading.Thread(target=self.poll_connection)
+        self.process_poll_socket_connect.start()
+
+    def poll_connection(self):
+        while True:
+            try:
+                self.sock.sendto(b"connect", (CORAL_IP, PORT))
+                data, _ = self.sock.recvfrom(CHUNK)
+                print(data)
+            except socket.timeout:  # Handle timeout exception
+                continue
+            except Exception as e:
+                print("Error while polling connection: ", e)
+                self.sock.close()
+                break
+
+            if data == b"connected":
+                print("Connected.")
+                self.append_output_signal.emit("BadMouth connection established, beginning stream...\n")
+                break
+
+        self.set_status_indicator_color("green")
+        try:
+            print("Opening pipe.")
+            with open(PIPE_PATH, "wb") as pipe:
+                self.set_status_indicator_color("green")
+                while True:
+                    data, _ = self.sock.recvfrom(CHUNK * 4)
+                    pipe.write(data)
+        except Exception as e:
+            print("Error while receiving audio stream: ", e)
+            self.append_output_signal.emit("Error while receiving audio stream: ", e)
+            self.set_status_indicator_color("red")
+            self.sock.close()
+
+    def update_running_indicator(self):
+        self.indicator_i += 1
+        new_txt = self.indicator_text[(self.indicator_i + 1) % 4]
+        self.running_indicator.setText(new_txt)
+
+        # Remove the current running indicator character
+        self.output_text.moveCursor(QTextCursor.End)
+        self.output_text.textCursor().deletePreviousChar()
+
+        # Add the updated running indicator character
+        self.output_text.moveCursor(QTextCursor.End)
+        self.output_text.insertPlainText(self.running_indicator.text())
+
+    def running_indicator_init(self):
+        # Initialize running indicator
+        self.running_indicator = QLabel()
+        self.indicator_text = "|/-\\"
+        self.indicator_i = 0
+        self.running_indicator.setText("|")
+
+        # Set up a timer to update the running indicator
+        self.running_indicator_timer = QTimer()
+        self.running_indicator_timer.timeout.connect(self.update_running_indicator)
+        self.running_indicator_timer.start(150)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
