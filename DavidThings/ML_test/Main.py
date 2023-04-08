4f014dc561257cc36e5ad28bbc6bb4117bc74dc7
David George <david.george@student.csulb.edu>
2023-04-08 16:35:14 -0700

    Sending

--- DavidThings/ML_test/Main.py
+++ DavidThings/ML_test/Main.py
@@ -12,7 +12,7 @@ import pyaudio
 import wave
 import shutil
 
-loaded_model = models.load_model("bad_mouth_1c_16n_0d_newData.model")
+#loaded_model = models.load_model("bad_mouth_1c_16n_0d_newData.model")
 
 
 def predict_mic():
@@ -25,7 +25,7 @@ def predict_mic():
     WAVE_OUTPUT_FILENAME = "recorded_file.wav"
 
     src_folder = "."
-    dst_folder = "C:/Users/David/Documents/ML Training Audios/ML_test/recorded_bad"
+    dst_folder = "C:/Users/David/Documents/BadMouth/DavidThings/ML_test/recorded_bad"
     #dst_folder = "C:/Users/David/Documents/ML Training Audios/ML_test/recorded_good"
 
     # initialize PyAudio
@@ -65,35 +65,35 @@ def predict_mic():
 
     os.rename(old_filename, new_filename)
 
-    pos = tf.data.Dataset.list_files(new_filename) 
+    # pos = tf.data.Dataset.list_files(new_filename) 
 
-    # append correct label to data
-    data = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.zeros(len(pos)))))
+    # # append correct label to data
+    # data = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.zeros(len(pos)))))
 
-    # apply preprocess to all the files 
-    data = data.map(preprocess)
+    # # apply preprocess to all the files 
+    # data = data.map(preprocess)
 
-    # process data in batches of 16
-    data = data.batch(16)
+    # # process data in batches of 16
+    # data = data.batch(16)
 
-    # get next batch
-    sample, labels = data.as_numpy_iterator().next()
+    # # get next batch
+    # sample, labels = data.as_numpy_iterator().next()
 
-    #print(sample.shape)
-    prediction = loaded_model.predict(sample)
+    # #print(sample.shape)
+    # prediction = loaded_model.predict(sample)
     
     
-    # iterate through the batch
-    for i in np.nditer(prediction): 
-        if (i > 0.9):
-            print("Bad Word  -- Prob:", i)
-        else:
-            print("Good word -- Prob:", i)
+    # # iterate through the batch
+    # for i in np.nditer(prediction): 
+    #     if (i > 0.9):
+    #         print("Bad Word  -- Prob:", i)
+    #     else:
+    #         print("Good word -- Prob:", i)
     shutil.move(os.path.join(src_folder, new_filename), os.path.join(dst_folder, new_filename))
 
-    # prediction = [1 if prediction > 0.99 else 0 for prediction in prediction]
+    # # prediction = [1 if prediction > 0.99 else 0 for prediction in prediction]
 
-    return prediction
+    # return prediction
 
 if __name__ == "__main__":
     # while True:
