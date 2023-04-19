2ea7224e3d6344b6d83dbf0ef1c5a7d49558b8a7
Tevin <107085383+WaywardRenegade@users.noreply.github.com>
2023-04-19 11:27:29 -0700

    Add files via upload
    manual addition of new test scripts for uart

--- BDSM/coral/Coral_Master.py
+++ BDSM/coral/Coral_Master.py
@@ -1,11 +1,16 @@
-#Coral Mini Master Code
+# Coral Mini Master Code
 import serial
-Rx_Pin = 10; #UART0 RX pin on the coral mini
-Tx_Pin = 8; #UART0 TX pin on the coral mini
+import time
 
-ser = serial.Serial('/dev/ttyS0',115200,timeout=0);#open serial communication at baud rate of 115200
-ser.write('Hello from the Coral Mini'.encode());#send data over UART0
-data = ser.read(10);#Read data from UART0 of the Pico
-print("Pico Received: ", data);
-ser.close()#close serial communication
+Rx_Pin = 10  # UART0 RX pin on the Coral Mini
+Tx_Pin = 8  # UART0 TX pin on the Coral Mini
 
+ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)  # Set the timeout to 1 second
+
+while True:
+    ser.write('Hello from the Coral Mini'.encode())  # Send data over UART0
+    time.sleep(0.5)  # Add a short delay before reading the response
+    data = ser.read(10)  # Read data from UART0 of the Pico
+    if data:
+        print("Pico Received: ", data)
+    time.sleep(1)  # Add a delay between each send-receive cycle
--- BDSM/pico/main.py
+++ BDSM/pico/main.py
@@ -1,15 +1,34 @@
-#python program for the pico slave mode
+# Python program for the Pico slave mode
 import machine
+from machine import Pin
 import utime
-Rx_Pin = 2 #UART0 of the pico Rx
-Tx_Pin = 3 #UART0 pf the pico Tx
-uart = machine.UART(1, baudrate = 115200, tx = machine.Pin(Tx_Pin), rx = machine.Pin(Rx_Pin))
-#creates uart object ^
-#loop to recieve and send data over UART
+
+#led = Pin(25, Pin.OUT)
+#Rx_Pin = 2  # UART0 of the pico Rx
+#Tx_Pin = 3  # UART0 of the pico Tx
+#uart = machine.UART(1, baudrate=115200, tx=machine.Pin(Tx_Pin), rx=machine.Pin(Rx_Pin))
+# Creates uart object ^
+
+# Loop to receive and send data over UART
+#while True:
+#    if uart.any():
+#        led.value(1)
+#        data = uart.read(10)
+#        print("Received Mini: ", data)
+#        # Send message back
+#        uart.write("Hello from Pico".encode())
+#        led.value(0)  # Turn off the LED after sending data
+#    utime.sleep_ms(100)
+
+Rx_Pin = 2  # UART0 of the Pico Rx
+Tx_Pin = 3  # UART0 of the Pico Tx
+uart = machine.UART(1, baudrate=115200, tx=machine.Pin(Tx_Pin), rx=machine.Pin(Rx_Pin))
+
 while True:
     if uart.any():
         data = uart.read(10)
-        print("Received Mini: ", data)
-        #send message back
-        wait.write("Hello from Pico".encode())
+        print("Received from Coral Mini: ", data)
+        uart.write(b'ACK')  # Send "ACK" back to the Coral Mini
     utime.sleep_ms(100)
+
+
