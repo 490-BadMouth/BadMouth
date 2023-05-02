5f364ed3c86dfcc4b006583c86d44a4717e2d10c
dmg77 <david.george@student.csulb.edu>
2023-05-01 17:18:47 -0700

    modified setup to add path

--- BadMouth_Gui/setup.sh
+++ BadMouth_Gui/setup.sh
@@ -4,6 +4,8 @@ export DISPLAY=:0.0
 # Display GUI on external LCD
 # Give permissions to send/receive UART
 uart_path="/dev/ttyS1"
-chmod 777 "$uart_path"
+mainwindow_path="/home/mendel/BadMouth/BadMouth_Gui/mainwindow.py"
+sudo chmod 777 "$uart_path"
+sudo chmod 777 "$mainwindow_path"
 # Run main window
-python3 mainwindow.py
+python3 /home/mendel/BadMouth/BadMouth_Gui/mainwindow.py
